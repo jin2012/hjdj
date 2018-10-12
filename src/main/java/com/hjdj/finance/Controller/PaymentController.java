afe54c49aca68e75a47789fdaf7f86e02a4b0975
jinshan <jinshan@jutoupay.com>
2018-10-12 16:43:26 +0800

    支付

--- src/main/java/com/hjdj/finance/Controller/PaymentController.java
+++ src/main/java/com/hjdj/finance/Controller/PaymentController.java
@@ -3,7 +3,6 @@ package com.hjdj.finance.Controller;
 import com.hjdj.finance.Constants.Constant;
 import com.hjdj.finance.beans.Order;
 import com.hjdj.finance.beans.Wxgzh;
-import com.hjdj.finance.init.Runner;
 import com.hjdj.finance.service.PaymentService;
 import com.hjdj.finance.service.WxgzhService;
 import com.hjdj.finance.utls.HttpUtil;
@@ -12,18 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.management.Query;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 @Controller
 public class PaymentController {
 
-
+    // 轮循次数
+    public static int val = 1;
 
     // 返回结果
     private String result;
@@ -114,43 +111,20 @@ public class PaymentController {
             return "redirect:" + P8_Url + "?result=" + result;
         }
         // 创建订单
-        Order order = new Order();
-        order.setP0_Cmd(P0_cmd);
-        order.setP1_MerId(P1_MerId);
-        order.setP2_Order(P2_Order);
-        order.setP3_Amt(P3_Amt);
-        order.setP4_Cur(P4_Cur);
-        order.setP5_Pid(P5_Pid);
-        order.setP6_Pcat(P6_Pcat);
-        order.setP7_Pdesc(P7_Pdesc);
-        order.setP8_Url(P8_Url);
-        order.setP9_SAF(P9_SAF);
-        order.setPa_MP(Pa_MP);
-        order.setPd_FrpId(Pd_FrpId);
-        order.setPr_NeedResponse(Pr_NeedResponse);
-        order.setHmac(Hmac);
-        // 后台使用，订单状态
-        order.setState("0");
-
+        Order order = getOrder(P8_Url, P0_cmd, P1_MerId, P2_Order, P3_Amt, P4_Cur, P5_Pid, P6_Pcat
+                , P7_Pdesc, P9_SAF, Pa_MP, Pd_FrpId, Pr_NeedResponse, Hmac);
         // 将订单数据存储到redis
         if (order == null || "".equals(order.getP2_Order())) {
             result = "OrderError";
             return "redirect:" + P8_Url + "?result=" + result;
         }
+        // 订单保存到Redis
         paymentService.saveOrderRedis(order);
-
+        // 从Redis查询wxgzh信息
         List<Wxgzh> wxgzhs = wxgzhService.queryWxgzhsRedis();
         Wxgzh wxgzh = wxgzhs.get(0);
         // 获取code路径
-        String codeUrl = "https://open.weixin.qq.com/connect/oauth2/authorize" +
-                // "?appid=wx979307632cb303ca" +
-                "?appid=" + wxgzh.getId() +
-                "&redirect_uri=http://topay.feigela.com/payment-pay/code" +
-                "&response_type=code" +
-                "&scope=snsapi_base" +
-                "&state=" + order.getP2_Order() +
-                "#wechat_redirect";
-
+        String codeUrl = getCodeUrl(order, wxgzh);
         return "redirect:" + codeUrl;
     }
 
@@ -162,8 +136,8 @@ public class PaymentController {
      * @return
      */
     @RequestMapping("/code")
-    public String obtainCode (HttpServletRequest request, HttpServletResponse response) {
-        /*String code = request.getParameter("code");
+    public String obtainCode(HttpServletRequest request, HttpServletResponse response) {
+        String code = request.getParameter("code");
         if (code == null || "".equals(code)) {
             return "";
         }
@@ -171,10 +145,9 @@ public class PaymentController {
         if (p2_Order == null || "".equals(p2_Order)) {
             return "";
         }
-
+        // 从redis中获取微信公众号信息
         List<Wxgzh> wxgzhs = wxgzhService.queryWxgzhsRedis();
         Wxgzh wxgzh = wxgzhs.get(0);
-
         // 获取openid路径
         String url = "https://api.weixin.qq.com/sns/oauth2/access_token";
         String param = "appid=" + wxgzh.getAppid() +
@@ -182,49 +155,119 @@ public class PaymentController {
                 "&code=" + code +
                 "&grant_type=authorization_code";
         String jsonStr = HttpUtil.sendGet(url, param);
-
+        // 获取返回的openid
         String openid = JSONObject.fromObject(jsonStr).getString("openid");
-
+        // 判断订单是否存在
         boolean isExist = paymentService.isRedisExist(p2_Order);
         if (isExist) {
             Order order = paymentService.query(p2_Order);
-            // 锋锐请求URL
-            StringBuilder sbd = new StringBuilder();
-            sbd.append("http://payment.feigela.com/GateWay/ReceiveBank.aspx");
-            sbd.append("?p0_Cmd=" + order.getP0_Cmd());
-            sbd.append("&p1_MerId=" + order.getP1_MerId());
-            sbd.append("&p2_Order=" + order.getP2_Order());
-            sbd.append("&p3_Amt=" + order.getP3_Amt());
-            sbd.append("&p4_Cur=" + order.getP4_Cur());
-            if (order.getP5_Pid() != null && !"".equals(order.getP5_Pid())) {
-                sbd.append("&p5_Pid=" + order.getP5_Pid());
-            }
-            if (order.getP6_Pcat() != null && !"".equals(order.getP6_Pcat())) {
-                sbd.append("&p6_Pcat=" + order.getP6_Pcat());
-            }
-            if (order.getP7_Pdesc() != null && !"".equals(order.getP7_Pdesc())) {
-                sbd.append("&p7_Pdesc=" + order.getP7_Pdesc());
-            }
-            sbd.append("&p8_Url=" + order.getP8_Url());
-            sbd.append("&p9_SAF=" + order.getP9_SAF());
-            if (order.getPa_MP() != null && !"".equals(order.getPa_MP())) {
-                sbd.append("&pa_MP=" + order.getPa_MP());
-            }
-            sbd.append("&pd_FrpId=" + order.getPd_FrpId());
-            sbd.append("&pr_NeedResponse=" + order.getPr_NeedResponse());
-            sbd.append("&hmac=" + order.getHmac());
-            sbd.append("&pr_Openid=" + openid);*/
-
-            lx();
-           /* return "redirect:" + sbd;
-        }*/
+            // 请锋锐请求URL
+            StringBuilder sbd = getFRUrl(openid, order);
+            // wxgzh轮循
+            wxgzhLX();
+            return "redirect:" + sbd;
+        }
         return "OrderOvertime";
     }
 
-    // 轮循次数
-    public static int val = 1;
+    /**
+     * 获取code路径
+     * @param order
+     * @param wxgzh
+     * @return
+     */
+    private String getCodeUrl(Order order, Wxgzh wxgzh) {
+        // 获取code路径
+        return "https://open.weixin.qq.com/connect/oauth2/authorize" +
+                // "?appid=wx979307632cb303ca" +
+                "?appid=" + wxgzh.getId() +
+                "&redirect_uri=http://topay.feigela.com/payment-pay/code" +
+                "&response_type=code" +
+                "&scope=snsapi_base" +
+                "&state=" + order.getP2_Order() +
+                "#wechat_redirect";
+    }
 
-    public synchronized void lx (){
+    /**
+     * 创建订单
+     *
+     * @param p8_Url
+     * @param p0_cmd
+     * @param p1_MerId
+     * @param p2_Order
+     * @param p3_Amt
+     * @param p4_Cur
+     * @param p5_Pid
+     * @param p6_Pcat
+     * @param p7_Pdesc
+     * @param p9_SAF
+     * @param pa_MP
+     * @param pd_FrpId
+     * @param pr_NeedResponse
+     * @param hmac
+     * @return
+     */
+    private Order getOrder(String p8_Url, String p0_cmd, String p1_MerId, String p2_Order, String p3_Amt, String p4_Cur, String p5_Pid, String p6_Pcat, String p7_Pdesc, String p9_SAF, String pa_MP, String pd_FrpId, String pr_NeedResponse, String hmac) {
+        Order order = new Order();
+        order.setP0_Cmd(p0_cmd);
+        order.setP1_MerId(p1_MerId);
+        order.setP2_Order(p2_Order);
+        order.setP3_Amt(p3_Amt);
+        order.setP4_Cur(p4_Cur);
+        order.setP5_Pid(p5_Pid);
+        order.setP6_Pcat(p6_Pcat);
+        order.setP7_Pdesc(p7_Pdesc);
+        order.setP8_Url(p8_Url);
+        order.setP9_SAF(p9_SAF);
+        order.setPa_MP(pa_MP);
+        order.setPd_FrpId(pd_FrpId);
+        order.setPr_NeedResponse(pr_NeedResponse);
+        order.setHmac(hmac);
+        // 后台使用，订单状态
+        order.setState("0");
+        return order;
+    }
+
+    /**
+     * 请求锋锐拼接路径
+     *
+     * @param openid
+     * @param order
+     * @return
+     */
+    private StringBuilder getFRUrl(String openid, Order order) {
+        StringBuilder sbd = new StringBuilder();
+        sbd.append("http://payment.feigela.com/GateWay/ReceiveBank.aspx");
+        sbd.append("?p0_Cmd=" + order.getP0_Cmd());
+        sbd.append("&p1_MerId=" + order.getP1_MerId());
+        sbd.append("&p2_Order=" + order.getP2_Order());
+        sbd.append("&p3_Amt=" + order.getP3_Amt());
+        sbd.append("&p4_Cur=" + order.getP4_Cur());
+        if (order.getP5_Pid() != null && !"".equals(order.getP5_Pid())) {
+            sbd.append("&p5_Pid=" + order.getP5_Pid());
+        }
+        if (order.getP6_Pcat() != null && !"".equals(order.getP6_Pcat())) {
+            sbd.append("&p6_Pcat=" + order.getP6_Pcat());
+        }
+        if (order.getP7_Pdesc() != null && !"".equals(order.getP7_Pdesc())) {
+            sbd.append("&p7_Pdesc=" + order.getP7_Pdesc());
+        }
+        sbd.append("&p8_Url=" + order.getP8_Url());
+        sbd.append("&p9_SAF=" + order.getP9_SAF());
+        if (order.getPa_MP() != null && !"".equals(order.getPa_MP())) {
+            sbd.append("&pa_MP=" + order.getPa_MP());
+        }
+        sbd.append("&pd_FrpId=" + order.getPd_FrpId());
+        sbd.append("&pr_NeedResponse=" + order.getPr_NeedResponse());
+        sbd.append("&hmac=" + order.getHmac());
+        sbd.append("&pr_Openid=" + openid);
+        return sbd;
+    }
+
+    /**
+     * 微信公众号轮循方法（线程安全）
+     */
+    public synchronized void wxgzhLX() {
         if (val < Constant.LX_VAL) {
             val++;
         } else {
--- src/main/java/com/hjdj/finance/utls/TestUtil.java
+++ src/main/java/com/hjdj/finance/utls/TestUtil.java
@@ -13,18 +13,18 @@ public class TestUtil {
         sValue.append("0.01");
         // 交易币种
         sValue.append("CNY");
-/*        // 商品名称
+        // 商品名称
         sValue.append("productname");
         // 商品种类
         sValue.append("producttype");
         // 商品描述
-        sValue.append("productdesc");*/
+        sValue.append("productdesc");
         // 商户接收支付成功数据的地址
         sValue.append("http://localhost:8080/massage.html");
         // 送货地址
         sValue.append("0");
-/*        // 商户扩展信息
-        sValue.append("123456");*/
+        // 商户扩展信息
+        sValue.append("123456");
         // 银行编码
         sValue.append("wxgzh");
         // 应答机制
